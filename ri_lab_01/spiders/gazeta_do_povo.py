fff370c7a2e330b0253034ff83a39293c6b77bcf
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-04-02 22:22:28 -0300

    removed the start_request method

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -17,13 +17,9 @@ class GazetaDoPovoSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        page = response.url.split(".")[1]
+        filename = '%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
