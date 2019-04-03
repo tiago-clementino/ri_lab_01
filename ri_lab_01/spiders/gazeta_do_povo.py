3ee8c6938c0219e6e51b53918ff50fe0f4e1988a
Ruan Roberto Eloy Silveira 115110981 <ruanres@lcc3-081.lcc.ufcg.edu.br>
2019-04-03 11:16:04 -0300

    implemented data colection and page navegation

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -18,6 +18,17 @@ class GazetaDoPovoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
+        for section in response.css('section.bl-post'):
+            yield {
+                "title": section.css('h1.c-titulo::text').get(),
+                "autor": section.css('ul.c-creditos a::attr(title)').get()
+            }
+
+        for next_url in response.css('article.c-chamada a::attr(href)').getall():
+            if next_url is not None:
+                yield scrapy.Request(next_url, callback=self.parse)
+
+
         page = response.url.split(".")[1]
         filename = '%s.html' % page
         with open(filename, 'wb') as f:
