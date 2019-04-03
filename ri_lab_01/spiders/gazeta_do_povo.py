cd7c754ea731e2349a5b71b3c54e5e66b2d035ab
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2019-04-02 21:41:45 -0300

    added method start_requests to spider

--- ri_lab_01/spiders/gazeta_do_povo.py
+++ ri_lab_01/spiders/gazeta_do_povo.py
@@ -17,15 +17,14 @@ class GazetaDoPovoSpider(scrapy.Spider):
                 data = json.load(json_file)
         self.start_urls = list(data.values())
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+
